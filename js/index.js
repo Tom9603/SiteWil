89c79292bb262fac0051b5c90e3149a6bcbc07b3
Tom9603 <tom.ochietti@gmail.com>
2025-05-17 14:18:56 +0200

    #

--- js/index.js
+++ js/index.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
    // Gère l'affichage du menu et du bouton "backToTop" au scroll
 window.addEventListener('scroll', () => {
     const scrollY = window.scrollY;
-    menu.classList.toggle('fixed', scrollY > 100);
+    menu.classList.toggle('fixed', scrollY > 0);
     menu.classList.toggle('scrolled', scrollY > 100);
     backToTop.classList.toggle('show', scrollY > 100);
 });
@@ -31,7 +31,7 @@ function isMobile() {
 
 // Scroll plus lent pour mobile
 function slowScrollToTop() {
-    const duration = 1200; // ralentir ici si tu veux encore plus doux
+    const duration = 1000; // ralentir ici si tu veux encore plus doux
     const start = window.scrollY;
     const startTime = performance.now();
 
